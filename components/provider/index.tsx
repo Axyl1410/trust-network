891ee2af3c41907a4e19edc8da4d180db593f623
Alex <truonggiang190689@gmail.com>
2025-07-22 03:29:04 +0700

    Refactor Provider component to remove thirdwebClient prop from ThirdwebProvider, simplifying the provider setup.

--- components/provider/index.tsx
+++ components/provider/index.tsx
@@ -3,11 +3,10 @@ import { Toaster } from "@/components/ui/sonner";
 import NextTopLoader from "nextjs-toploader";
 import * as React from "react";
 import { ThirdwebProvider } from "thirdweb/react";
-import { thirdwebClient } from "@/lib/thirdweb";
 
 export function Provider({ children }: { children: React.ReactNode }) {
 	return (
-		<ThirdwebProvider client={thirdwebClient}>
+		<ThirdwebProvider>
 			<Toaster closeButton position="bottom-right" />
 			<NextTopLoader showSpinner={false} zIndex={5000} />
 			{children}
